24a810f0619c9c431f914d66064ae323f4fc085c
Chamath Jayarathne <chamathsocial@gmail.com>
2023-09-08 14:38:29 -0400

    feat: Read data from csv file, add functionality to buttons

---
+++ data_manager.py
@@ -0,0 +1,34 @@
+import pandas
+import random
+
+FR = "French"
+EN = "English"
+
+
+class DataManager:
+    def __init__(self):
+        self.data = None
+        self.word_list = []
+        self.pull_data()
+        self.correct_list = []
+        self.fr_word = None
+        self.en_word = None
+        self.get_word()
+
+    def pull_data(self):
+        data = pandas.read_csv("./data/french_words.csv")
+        self.word_list = data.to_dict(orient="records")
+
+    def get_word(self):
+        word = random.choice(self.word_list)
+        self.fr_word = word[FR]
+        self.en_word = word[EN]
+
+        print(self.correct_list)
+
+    def got_correct(self):
+        self.correct_list.append(self.fr_word)
+        self.get_word()
+
+    def got_wrong(self):
+        self.get_word()
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from tkinter import *
+from data_manager import DataManager
 
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Ariel"
-LANG = "French"
-word = "Word"
+TITLE = "French"
 
-# UI SETUP
+word_manager = DataManager()
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+# Update Word
+def update_word():
+    canvas.itemconfig(word_text, text=word_manager.fr_word)
+
+
+# Handle correct button
+def correct_click():
+    word_manager.got_correct()
+    update_word()
+
+
+# Handle wrong button
+def wrong_click():
+    word_manager.got_wrong()
+    update_word()
+
 
 # Window
 window = Tk()
@@ -18,18 +38,18 @@ canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0
 front_card_img = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=front_card_img)
 # Canvas texts
-title_text = canvas.create_text(400, 150, text=LANG, font=(FONT_NAME, 40, "italic"))
-word_text = canvas.create_text(400, 263, text=word, font=(FONT_NAME, 60, "bold"))
+title_text = canvas.create_text(400, 150, text=TITLE, font=(FONT_NAME, 40, "italic"))
+word_text = canvas.create_text(400, 263, text=word_manager.fr_word, font=(FONT_NAME, 60, "bold"))
 
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Buttons
 correct_img = PhotoImage(file="./images/right.png")
-correct_btn = Button(image=correct_img, highlightthickness=0, border=0)
+correct_btn = Button(image=correct_img, command=correct_click, highlightthickness=0, border=0)
 correct_btn.grid(column=1, row=1)
 
 wrong_img = PhotoImage(file="./images/wrong.png")
-wrong_btn = Button(image=wrong_img, highlightthickness=0, border=0)
+wrong_btn = Button(image=wrong_img, command=wrong_click, highlightthickness=0, border=0)
 wrong_btn.grid(column=0, row=1)
 
 window.mainloop()
